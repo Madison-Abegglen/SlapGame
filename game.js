2ee4be4e5de0ceb12480e12febd90552c245f415
Madison Abegglen <madison.abegglen@gmail.com>
2018-08-06 12:34:00 -0600

    lots of things

--- game.js
+++ game.js
@@ -1,59 +1,75 @@
-const gnoll = {
-    name: 'Angry Gnoll',
-    hits: 0,
-    health: 100,
-    img: 'images/gnoll.brick.png'
-}
-
-const knight = {
-    name: 'Evil Knight',
-    hits: 0,
-    health: 125,
-    img: 'images/knightStatue.brick.png'
-}
-
-const ghost = {
-    name: 'Peeved Ghost',
-    hits: 0,
-    health: 150,
-    img: 'images/wraith.brick.png'
-}
-
-
-var name = 'Bork'
-var health = 100
-var hits = 0
-
 function update() {
     if (health < 0) {
         health = 0;
-        document.getElementById('slap').disabled = true;
-        document.getElementById('punch').disabled = true;
-        document.getElementById('stab').disabled = true;
+        document.getElementById('slap');
+        document.getElementById('punch');
+        document.getElementById('stab');
     }
-    document.getElementById('health').innerText = health;
-    document.getElementById('hits').innerText = hits;
-    document.getElementById('name').innerText = name;
+    document.getElementById('health').innerText = target.health;
+    document.getElementById('hits').innerText = target.hits;
+    document.getElementById('name').innerText = target.name;
+}
+
+let items = {
+    fire: { name: 'Fire', modifier: 2, description: 'Fire will burn most things' },
+    poison: { name: 'Poison', modifier: 5, description: 'And you thought food poisoning was bad' },
+    bleeding: { name: 'Bleeding', modifier: 10, description: 'Yep, thats gonna need some medical attention' }
+}
+
+let target = {
+    name: "Bork the Gnoll",
+    health: 100,
+    hits: 0,
+    items: []
+}
+
+function reset() {
+    target = {
+        name: "Bork the Gnoll",
+        health: 100,
+        hits: 0,
+        items: []
+    }
+    update();
+}
+
+function giveFire() {
+    target.items.push(items.fire);
+}
+
+function givePoison() {
+    target.items.push(items.poison);
+}
+
+function giveBleeding() {
+    target.items.push(items.bleeding);
+}
+
+function addMods() {
+    let total = 0;
+    for (let i = 0; i < target.items.length; i++) {
+        let currentItem = target.items[i]
+        total += currentItem.modifier
+    }
+    return total;
 }
 
 function slap() {
-    health -= 1;
-    hits += 1;
+    target.health -= 1 + addMods();
+    target.hits += 1;
     update()
 }
 
 function punch() {
-    health -= 5;
-    hits += 1;
+    target.health -= 5 + addMods();
+    target.hits += 1;
     update()
 }
 
 function stab() {
-    health -= 10;
-    hits += 1;
+    target.health -= 10;
+    target.hits += 1;
     update()
 }
-
-
 
 update()
